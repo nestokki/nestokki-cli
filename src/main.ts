071ed9cecbc675db7eb50be65d304733577310a6
okonomiyakki <parkji12532@naver.com>
2025-11-15 21:09:49 +0900

    fix: 버전 조회 커맨드

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,13 @@
-declare const process: {
-  argv: string[];
-  exit(code?: number): never;
-};
-
+#!/usr/bin/env node
 import { Command } from 'commander';
 import pkg from '../package.json';
 
 const program = new Command();
-const version = pkg.version ?? '0.0.0';
+const version = pkg.version ?? '1.0.0';
 
 program
   .name('nestokki')
   .description('Nest CLI helper for file autocompletion.')
-  .version(version, '-v, --version', 'display CLI version');
-
-const rawArgs = process.argv.slice(2);
-if (rawArgs.includes('-version')) {
-  console.log(version);
-  process.exit(0);
-}
+  .version(version, '-v, --version', 'Display CLI version');
 
 program.parse(process.argv);
