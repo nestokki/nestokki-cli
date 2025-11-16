a881835a39c5880d311dee1ba161633523804431
okonomiyakki <parkji12532@naver.com>
2025-11-16 15:04:08 +0900

    feat: 문자열 포멧 변환 유틸

---
+++ src/util/convert-case.util.ts
@@ -0,0 +1,33 @@
+export const toPascalCase = (name: string): string => {
+  return name
+    .replace(/[_\s]+/g, '-')
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
+    .split('-')
+    .filter(Boolean)
+    .map((part) => part.toLowerCase())
+    .map((part) => part[0].toUpperCase() + part.slice(1))
+    .join('');
+};
+
+export const toKebabCase = (name: string): string => {
+  return name
+    .replace(/[_\s]+/g, '-')
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
+    .split('-')
+    .filter(Boolean)
+    .map((part) => part.toLowerCase())
+    .join('-');
+};
+
+export const toSnakeCase = (name: string): string => {
+  return name
+    .replace(/[_\s]+/g, '-')
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
+    .split('-')
+    .filter(Boolean)
+    .map((part) => part.toLowerCase())
+    .join('_');
+};
