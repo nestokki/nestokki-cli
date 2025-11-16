76a0e55256325d35704180921df65f16f37c3968
okonomiyakki <parkji12532@naver.com>
2025-11-16 15:06:48 +0900

    feat: 엔티티 파일 생성 함수

---
+++ src/generator/infrastructure/generate-entity.ts
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import path from 'path';
+import ora from 'ora';
+import { toPascalCase, toKebabCase, toSnakeCase } from '../../util/convert-case.util';
+import { logConflictError, logCreated, logFailure } from '../../util/log-style.util';
+
+export const generateEntity = (domainName: string): void => {
+  const spinner = ora(`Generating for ${domainName}...\n`).start();
+
+  const pascal = toPascalCase(domainName);
+  const kebab = toKebabCase(domainName);
+  const snake = toSnakeCase(domainName);
+
+  const entityClassName = `${pascal}Entity`;
+  const tableName = snake;
+
+  try {
+    const cwd = process.cwd();
+
+    const moduleDir = path.join(cwd, 'src', kebab);
+    const entityDir = path.join(moduleDir, 'infrastructure', 'entity');
+
+    fs.mkdirSync(entityDir, { recursive: true });
+
+    const entityFilePath = path.join(entityDir, `${kebab}.entity.ts`);
+
+    if (fs.existsSync(entityFilePath)) {
+      throw logConflictError(entityClassName, path.relative(cwd, entityFilePath));
+    }
+
+    const templatePath = path.join(__dirname, '../../template/entity.hbs');
+    const template = fs.readFileSync(templatePath, 'utf8');
+
+    const content = template
+      .replace(/{{tableName}}/g, tableName)
+      .replace(/{{entityClassName}}/g, entityClassName);
+
+    fs.writeFileSync(entityFilePath, content, { encoding: 'utf8' });
+
+    spinner.succeed(logCreated(entityClassName, path.relative(cwd, entityFilePath)));
+  } catch (error: unknown) {
+    if (typeof error === 'string') spinner.fail(error);
+    else spinner.fail(logFailure(entityClassName));
+    throw error;
+  }
+};
