df890aa54ec17ef3bb2cc0dbcc177d0acc705825
okonomiyakki <parkji12532@naver.com>
2025-11-23 22:33:56 +0900

    feat: 카멜 케이스 변환 유틸 함수

--- src/util/convert-case.util.ts
+++ src/util/convert-case.util.ts
@@ -31,3 +31,17 @@ export const toSnakeCase = (name: string): string => {
     .map((part) => part.toLowerCase())
     .join('_');
 };
+
+export const toCamelCase = (name: string): string => {
+  return name
+    .replace(/[_\s]+/g, '-')
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
+    .split('-')
+    .filter(Boolean)
+    .map((part, i) => {
+      const lower = part.toLowerCase();
+      return i === 0 ? lower : lower[0].toUpperCase() + lower.slice(1);
+    })
+    .join('');
+};
