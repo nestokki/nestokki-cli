1dbd9e7bc7d15da84f3757fb4d4454f3853c8bd3
okonomiyakki <parkji12532@naver.com>
2025-11-23 23:31:16 +0900

    feat: 도메인 프러퍼티 타입 파일 생성 함수

---
+++ src/generator/domain/props-interface.generator.ts
@@ -0,0 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import ora from 'ora';
+import { toPascalCase, toKebabCase } from '../../util/convert-case.util';
+import { logCreated, logFailure, logSkipped } from '../../util/log-style.util';
+
+export const generatePropsInterface = (domainName: string): void => {
+  const domainNamePascal = toPascalCase(domainName);
+  const domainNameKebab = toKebabCase(domainName);
+
+  const propsInterfaceName = `${domainNamePascal}PropsInterface`;
+
+  const spinner = ora(`Generating for ${propsInterfaceName}...\n`).start();
+
+  try {
+    const cwd = process.cwd();
+
+    const moduleDir = path.join(cwd, 'src', 'api', domainNameKebab);
+    const propsInterfaceDir = path.join(moduleDir, 'domain', 'model');
+
+    fs.mkdirSync(propsInterfaceDir, { recursive: true });
+
+    const domainFilePath = path.join(propsInterfaceDir, `${domainNameKebab}-props.interface.ts`);
+    const relativePath = path.relative(cwd, domainFilePath);
+
+    if (fs.existsSync(domainFilePath)) {
+      spinner.info(logSkipped(propsInterfaceName, relativePath));
+      return;
+    }
+
+    const templatePath = path.join(__dirname, '../../template/props-interface.hbs');
+    const template = fs.readFileSync(templatePath, 'utf8');
+
+    const content = template.replace(/{{domainNamePascal}}/g, domainNamePascal);
+
+    fs.writeFileSync(domainFilePath, content, { encoding: 'utf8' });
+
+    spinner.succeed(logCreated(propsInterfaceName, relativePath));
+  } catch (error: unknown) {
+    spinner.fail(logFailure(propsInterfaceName));
+    throw error;
+  }
+};
