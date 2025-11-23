3081a2236598de15b71e463302174d33329a0bf6
okonomiyakki <parkji12532@naver.com>
2025-11-23 22:57:56 +0900

    feat: 도메인 파일 생성 함수

---
+++ src/generator/domain/domain.generator.ts
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import path from 'path';
+import ora from 'ora';
+import { toPascalCase, toKebabCase } from '../../util/convert-case.util';
+import { logCreated, logFailure, logSkipped } from '../../util/log-style.util';
+
+export const generateDomain = (domainName: string): void => {
+  const domainNamePascal = toPascalCase(domainName);
+  const domainNameKebab = toKebabCase(domainName);
+
+  const domainClassName = `${domainNamePascal}Domain`;
+
+  const spinner = ora(`Generating for ${domainClassName}...\n`).start();
+
+  try {
+    const cwd = process.cwd();
+
+    const moduleDir = path.join(cwd, 'src', 'api', domainNameKebab);
+    const domainDir = path.join(moduleDir, 'domain', 'model');
+
+    fs.mkdirSync(domainDir, { recursive: true });
+
+    const domainFilePath = path.join(domainDir, `${domainNameKebab}.domain.ts`);
+    const relativePath = path.relative(cwd, domainFilePath);
+
+    if (fs.existsSync(domainFilePath)) {
+      spinner.info(logSkipped(domainClassName, relativePath));
+      return;
+    }
+
+    const templatePath = path.join(__dirname, '../../template/entity.hbs');
+    const template = fs.readFileSync(templatePath, 'utf8');
+
+    const content = template
+      .replace(/{{domainClassName}}/g, domainClassName)
+      .replace(/{{domainNamePascal}}/g, domainNamePascal)
+      .replace(/{{domainNameKebab}}/g, domainNameKebab);
+
+    fs.writeFileSync(domainFilePath, content, { encoding: 'utf8' });
+
+    spinner.succeed(logCreated(domainClassName, relativePath));
+  } catch (error: unknown) {
+    spinner.fail(logFailure(domainClassName));
+    throw error;
+  }
+};
