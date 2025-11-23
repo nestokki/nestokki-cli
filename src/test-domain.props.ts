95b806b7854d9d477faaa3d9dd65efb4b60217a4
okonomiyakki <parkji12532@naver.com>
2025-11-23 19:38:50 +0900

    feat: 도메인 프러퍼티 타입 샘플

---
+++ src/test-domain.props.ts
@@ -0,0 +1,35 @@
+/** Example domain types (remove these in real use!) */
+type ManyToOneDomain = {};
+type OneToManyDomain = {};
+
+interface UserPk {
+  idx: number;
+}
+
+interface UserRequiredProps {
+  required: string;
+}
+
+interface UserNullableProps {
+  nullable: string | null;
+}
+
+interface UserDefaultProps {
+  updatedAt: Date;
+  createdAt: Date;
+}
+
+interface UserRelationProps {
+  manyToOne?: ManyToOneDomain | null;
+  oneToMany?: readonly OneToManyDomain[];
+}
+
+export type UserDomainProps = UserPk &
+  UserRequiredProps &
+  UserNullableProps &
+  UserDefaultProps &
+  UserRelationProps;
+
+export type UserCreateProps = UserRequiredProps & UserNullableProps;
+
+export type UserUpdateProps = Partial<UserRequiredProps> & Partial<UserNullableProps>;
