2cf60fba8352ce1dc9220908a1047c387bd24e5e
okonomiyakki <parkji12532@naver.com>
2025-11-24 22:30:56 +0900

    feat: 관계 설정 기준, 대상 모듈 선택 옵션 + 조인 옵션

--- src/prompt/prompt-type.indicator.ts
+++ src/prompt/prompt-type.indicator.ts
@@ -1,4 +1,9 @@
-import { GenerationCategory, LayerCategory } from '../common/enum';
+import {
+  GenerationCategory,
+  LayerCategory,
+  OnDeleteCategory,
+  RelationCategory,
+} from '../common/enum';
 import { CheckableChoices, Choices } from '../common/interface';
 
 export const getGenerationTypeChoices = (): Choices[] => {
@@ -23,6 +28,30 @@ export const getFeatureLayerTypeChoices = (domainName: string): CheckableChoices
     { name: 'Presentation (controller)', value: LayerCategory.PRESENTATION },
   ];
 };
+
+export const getRelationBaseModuleChoices = (modules: string[]): CheckableChoices[] => {
+  return modules.map((module) => ({ name: module, value: module }));
+};
+
+export const getRelationTargetModuleChoices = (modules: string[], base: string) => {
+  return modules
+    .filter((module) => module !== base)
+    .map((target) => ({ name: target, value: target }));
+};
+
+export const getRelationTypeChoices = (): CheckableChoices[] => {
+  return [
+    { name: 'ManyToOne', value: RelationCategory.MANY_TO_ONE },
+    { name: 'OneToMany', value: RelationCategory.ONE_TO_MANY },
+    { name: 'OneToOne', value: RelationCategory.ONE_TO_ONE },
+  ];
+};
+
+export const getOnDeleteChoices = () => [
+  { name: 'CASCADE', value: OnDeleteCategory.CASCADE },
+  { name: 'SET NULL', value: OnDeleteCategory.SET_NULL },
+  { name: 'RESTRICT', value: OnDeleteCategory.RESTRICT },
+];
 
 // export const getDatabaseOrmTypeChoices = (): CheckableChoices[] => {
 //   return [
